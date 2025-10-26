cb337d31598c320e3ac4e014a991636da7fb1f8d
Fuslone <230797359+Fuslone@users.noreply.github.com>
2025-10-26 21:30:51 +0800

    Add files via upload

---
+++ script/search.js
@@ -0,0 +1,39 @@
+function search(type,input){
+    for (let item in type){
+        if (typeof type[item]==='object'){
+            search(type[item],input);
+        }else{
+            if (type[item]?.toLowerCase().includes(input)){
+                resultNumber++;
+                result.push(type);
+                return;
+            }
+        }
+    }
+}
+let result=[];
+let resultNumber=0;
+const form=(new URL(window.location.href)).searchParams;
+if (form.size>0){
+    const type=form.get('type');
+    const input=form.get('search').toLowerCase();
+fetch('search.json')
+.then(response=>{
+    if (!response.ok){
+        throw new Error(`错误: ${response.status}`);
+    }
+    return response.json();
+}).then(data=>{
+    if (type==='all'){
+        search(data.document,input);
+        search(data.link,input);
+        search(data.project,input);
+        search(data.software,input);
+    }else{
+        search(data[`${type}`],input);
+    }
+    console.log(result);
+}).catch(error=>{
+    console.error(error);
+});
+}
